7dd931390b71494eb9f12454ba314ada096e3f6c
Kamil <80158455+kamilego@users.noreply.github.com>
2022-03-17 22:22:37 +0100

    create new tab with interesting characters

--- app.py
+++ app.py
@@ -5,14 +5,17 @@ import random
 
 app=Flask(__name__)
 
+
 @app.route('/')
 def index():
     text = open('dane/xd.txt').read()
     return render_template("index.html", text=text)
 
+
 @app.route('/xd')
 def xd():
     return render_template("xd.html")
+
 
 @app.route('/brudnopis')
 def brudnopis():
@@ -24,13 +27,34 @@ def brudnopis():
     bruce = wiki_char("Mikołaj Kopernik")
     return render_template("brudnopis.html", hero1=new_hero, hero2=hero2, hero3=champ_choice, a=a, poem=poem, bruce=bruce)
 
+
 @app.route('/byczek')
 def byczek():
     return render_template("byczek.html")
 
+
 @app.route('/kamilek')
 def kamilek():
-    return "Kocham moją ukochaną Olę :**"
+    return "Kocham moją ukochaną Olę\n :**"
+
+
+@app.route('/kochanaola', methods=['GET', 'POST'])
+def mylove(): 
+    return render_template('kochanaola.html')
+
+
+@app.route('/ciekawe-postacie')
+def ciekawe_postacie():
+    int_character = ["Małysz", "Kopernik", "Maria Skłodowska", "Donald Trump", "Kościuszko", "Mieszko I"]
+    rand_chars = random.sample(int_character, 3)
+    char_descr = []
+    for char in rand_chars:
+        description = wiki_char(char)
+        char_descr.append([char, description, len(description), len(description.split(" "))])
+    char_descr = sorted(char_descr, key=lambda x: x[3], reverse=True)
+    return render_template("ciekawe-postacie.html", chars=char_descr)
+
+
 
 if __name__=="__main__":
     app.run()
